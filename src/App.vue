dbfe487120ae59821eafac9431a658ca85ba952f
Sergii Bida <sergijbida15@gmail.com>
2024-08-09 19:29:18 +0300

    did tasks 1-3

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script>
+import User from '@/components/User.vue';
+
+export default {
+  name: 'App',
+  components: {
+    User
+  },
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
+
+<template>
+  <main>
+    <!--tasks 1-3-->
+    <User/>
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Products.vue
@@ -0,0 +1,12 @@
+<script setup>
+import {reactive} from "vue";
+
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
